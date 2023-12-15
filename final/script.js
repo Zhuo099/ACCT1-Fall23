a2b556b31040cb3eb538c0fa610426facef930bf
Zhuo099 <145067168+Zhuo099@users.noreply.github.com>
2023-12-14 20:27:16 -0800

    1
    1

---
+++ final/script.js
@@ -0,0 +1,61 @@
+// 获取标题元素
+var arduinoTitle = document.getElementById('1-title');
+var processingTitle = document.getElementById('2-title');
+var prototypeTitle = document.getElementById('3-title');
+var videoTitle = document.getElementById('4-title');
+
+// 标记当前显示的元素状态
+var isShowingImages = true;
+var isShowingVideo = false;
+
+arduinoTitle.addEventListener('click', function() {
+    showImages('left');
+});
+
+processingTitle.addEventListener('click', function() {
+    showImages('right');
+});
+
+prototypeTitle.addEventListener('click', function() {
+    showImages('proto');
+});
+
+videoTitle.addEventListener('click', function() {
+    toggleVideo();
+});
+
+function showImages(type) {
+    var allImages = document.querySelectorAll('.images-container img');
+    var leftImages = document.querySelectorAll('.left-image');
+    var rightImages = document.querySelectorAll('.right-image');
+    var protoImages = document.querySelectorAll('.proto-image');
+    var videoContainer = document.querySelector('.video-container');
+
+    videoContainer.style.display = 'none'; // 隐藏视频
+    isShowingVideo = false;
+    allImages.forEach(img => img.style.display = 'none');
+
+    if (type === 'left') {
+        leftImages.forEach(img => img.style.display = 'block');
+    } else if (type === 'right') {
+        rightImages.forEach(img => img.style.display = 'block');
+    } else if (type === 'proto') {
+        protoImages.forEach(img => img.style.display = 'block');
+    }
+    isShowingImages = true;
+}
+
+function toggleVideo() {
+    var allImages = document.querySelectorAll('.images-container img');
+    var videoContainer = document.querySelector('.video-container');
+
+    if (!isShowingVideo) {
+        allImages.forEach(img => img.style.display = 'none'); // 隐藏所有图片
+        videoContainer.style.display = 'block'; // 显示视频
+        isShowingVideo = true;
+    } else {
+        videoContainer.style.display = 'none'; // 隐藏视频
+        isShowingVideo = false;
+    }
+    
+}
